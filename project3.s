397bd1e5dea32035fe96e8c79051783cf0ab4af9
unknown <a.adhikari6960@gmail.com>
2020-12-02 18:05:10 +0545

    Pointers have been added to traverse through the list.

--- project3.s
+++ project3.s
@@ -7,3 +7,6 @@
             la $a0, reply
             li $a1, 1001
             syscall
+	    la $s0, reply		#Loading the adress of input to the first permanent value storing register $s0 and to work through the string.
+            li $s1, 0 			#start pointer to make sure that we collect only 4 characters and a comma.
+            li $s2, 0			#end pointer to make sure what characters we have collected and read.
